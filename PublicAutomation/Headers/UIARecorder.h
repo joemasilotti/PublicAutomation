5fc0af27e82fa243ea17c028daf1eeb2ffe4c04b
Joe Masilotti <joseph.masilotti@gmail.com>
2014-11-20 06:38:16 -0500

    Clean up dumped class imports

--- PublicAutomation/Headers/UIARecorder.h
+++ PublicAutomation/Headers/UIARecorder.h
@@ -4,9 +4,8 @@
 //     class-dump is Copyright (C) 1997-1998, 2000-2001, 2004-2013 by Steve Nygard.
 //
 
-#import "NSObject.h"
-
 @class NSArray, NSMutableString, NSTimer, UIARecorderAction, UIATarget, UIAXElement;
+@protocol UIARecorderDelegate;
 
 @interface UIARecorder : NSObject
 {
